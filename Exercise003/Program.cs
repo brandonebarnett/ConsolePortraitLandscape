00044b0b2da019c1123507673c03b09eee15be6f
Brandon <brandon.ebarnett@gmail.com>
2022-08-27 10:08:39 -0400

    Add project files.

---
+++ Exercise003/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Write a program that askes the user to input a width and height of an image and tell whether the image is portrait or landscape. */
+
+Console.WriteLine("--------------------------------------------------------------------");
+Console.WriteLine("-----------------------Portrait or Landscape------------------------");
+Console.WriteLine("--------------------------------------------------------------------");
+Console.WriteLine();
+Console.Write("Please enter the width of the image: ");
+int imageWidth = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Please enter the height of the image: ");
+int imageHeight = Convert.ToInt32(Console.ReadLine());
+
+if (imageHeight > imageWidth)
+    Console.WriteLine("Your image is portrait.");
+else
+    Console.WriteLine("Your image is landscape.");
